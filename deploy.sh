ab46f72947e1daf1dc2bf65c155993fb28966778
Karan Gupta <gupta.karan.11@gmail.com>
2018-10-30 00:59:08 +0530

    rebuilding site Tue, Oct 30, 2018 12:59:06 AM

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,19 @@
 
 set -e
 
+echo -e "\033[0;32mCleaning up...\033[0m"
+
+# Clean the public folder.
+if [ -d public ]; then
+  rm -r public
+fi
+
 echo -e "\033[0;32mPulling changes from GitHub...\033[0m"
 
 # pull changes
 git pull
 
 echo -e "\033[0;32mRebuilding site...\033[0m"
-
-# Clean the public folder.
-if [ -d public ]; then
-  rm -r public
-fi
 
 # Build the project.
 hugo
