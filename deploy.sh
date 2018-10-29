8339f5794180d324c882c56964d8a6d90e95014e
Karan Gupta <gupta.karan.11@gmail.com>
2018-10-29 19:35:58 +0530

    rebuilding site Mon, Oct 29, 2018  7:35:58 PM

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+set -e
+
+echo -e "\033[0;32mPulling changes from GitHub...\033[0m"
+
+# pull changes
+git pull
+
+echo -e "\033[0;32mRebuilding site...\033[0m"
 
 # Clean the public folder.
-rm -r public
+if [ -d public ]; then
+  rm -r public
+fi
 
 # Build the project.
 hugo
@@ -18,6 +27,10 @@ if [ $# -eq 1 ]
 fi
 git commit -m "$msg"
 
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
 # Push source and build repos.
 git push origin master
 git subtree push --prefix=public git@github.com:grasskode/grasskode-website.git gh-pages
+
+set +e
