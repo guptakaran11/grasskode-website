77f5bf39cd555b99c6a99e943828d075fc26d920
Karan Gupta <gupta.karan.11@gmail.com>
2018-10-30 01:58:31 +0530

    uncommitted changes Tue Oct 30 01:58:31 IST 2018

--- deploy.sh
+++ deploy.sh
@@ -24,18 +24,25 @@ git subtree add --prefix=public git@github.com:grasskode/grasskode-website.git g
 # Build the project.
 hugo
 
-# Add and commit site changes to git if any.
 set +e
+set -x
+
+echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+# Push source and build repos.
+mv .gitignore .gitignore-master
+mv .gitignore-gh-pages .gitignore
+git subtree push --prefix=public git@github.com:grasskode/grasskode-website.git gh-pages
+mv .gitignore .gitignore-gh-pages
+mv .gitignore-master .gitignore
+
+# Add and commit site changes to git if any.
 git add -A
 msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
 git commit -m "$msg"
-
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
-
-# Push source and build repos.
 git push origin master
-git subtree push --prefix=public git@github.com:grasskode/grasskode-website.git gh-pages
 
+set +x
