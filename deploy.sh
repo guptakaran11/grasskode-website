2dacc5e9758c79bce2520ddf532f61dde347a75e
Karan Gupta <gupta.karan.11@gmail.com>
2018-10-30 02:17:44 +0530

    uncommitted changes Tue Oct 30 02:17:44 IST 2018

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,12 @@ echo -e "\033[0;32mUpdating master...\033[0m"
 msg="uncommitted changes `date`"
 git commit -am "$msg"
 
-echo -e "\033[0;32mPulling changes from GitHub...\033[0m"
-
 # pull changes
 git pull
+
+# push changes
+git push origin master
+
 
 echo -e "\033[0;32mRebuilding site...\033[0m"
 
@@ -20,23 +22,32 @@ if [ -d public ]; then
 fi
 git subtree add --prefix=public git@github.com:grasskode/grasskode-website.git gh-pages
 
+# swap .gitignore
+mv .gitignore .gitignore-master
+mv .gitignore-gh-pages .gitignore
+
 # Build the project.
 hugo
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
 # Add and commit site changes to git if any.
-git add -A
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-git push origin master
+#git add -A
+#msg="rebuilding site `date`"
+#if [ $# -eq 1 ]
+#  then msg="$1"
+#fi
+#git commit -m "$msg"
+#git push origin master
 
 # Push source and build repos.
-mv .gitignore .gitignore-master
-mv .gitignore-gh-pages .gitignore
+git add -A
+msg="rebuilding site `date`"
+#if [ $# -eq 1 ]
+#  then msg="$1"
+#fi
+git commit -m "$msg"
+#git push origin master
 git subtree push --prefix=public git@github.com:grasskode/grasskode-website.git gh-pages
 mv .gitignore .gitignore-gh-pages
 mv .gitignore-master .gitignore
